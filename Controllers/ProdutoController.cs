8787f94f4e37aa9d7152f6614f27aaff54040074
LEONARDO LUIZ SCHROEDER <leonardoschroeder@univille.br>
2019-09-17 18:59:02 -0300

    inicio crud

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -5,13 +5,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using myshop.Models;
+using myshop.Services;
 
 namespace myshop.Controllers
 {
     public class ProdutoController : Controller
     {
+        private IProdutoServico produtoServico;
+
+        public ProdutoController(IProdutoServico _produtoServico)
+        {
+            produtoServico = _produtoServico;
+        }
+
         public IActionResult Index()
         {
+            /* var listaProduto = new List<Produto>();
+            listaProduto = produtoServico.BuscarTodos();
+            return View(listaProduto);*/
+            Console.WriteLine("testando o codigo01");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Produto produto){
+            Console.WriteLine("Nome");
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine("Preco");
+            Console.WriteLine(produto.Preco);
+            Console.WriteLine("Preco Promocional");
+            Console.WriteLine(produto.PrecoPromocional);
+            Console.WriteLine("Descrição");
+            Console.WriteLine(produto.Descricao);
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form(){
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Produto produto){
+            Console.WriteLine("Nome");
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine("Preco");
+            Console.WriteLine(produto.Preco);
+            Console.WriteLine("Preco Promocional");
+            Console.WriteLine(produto.PrecoPromocional);
+            Console.WriteLine("Descrição");
+            Console.WriteLine(produto.Descricao);
+            produtoServico.InserirProduto(produto);
             return View();
         }
 
